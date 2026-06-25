a1a5ea4c89434141775f4b056edc0d8876012679
carlospolicarpio9158-lgtm <carlospolicarpio9158@gmail.com>
2026-06-25 21:13:18 +0800

    Create Gacha.java

---
+++ SmallProjects/Gacha.java
@@ -0,0 +1,13 @@
+ import java.util.Random;
+    import java.util.Scanner;
+
+    public class Gacha {
+        public static void main (String[] args) {
+            Scanner s = new Scanner (System.in);
+            Random r = new Random();
+
+            int rcount = 0;
+            int srcount = 0;
+            int ssrcount = 0;
+            int urcount = 0;
+            int uurcount = 0;
